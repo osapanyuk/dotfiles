d6cb97ad8851fc94d7ee3369b9a55d0aa0f37bfc
Oleg Sapanyuk <oleg.sapanyuk@gmail.com>
2024-01-22 21:24:30 -0500

    dotfile init

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,13 @@
+-- Lua
+require('onedark').setup {
+    style = 'deep',
+    transparent = true,
+    colors = {
+	blue = '#4ec9ff'
+    },
+    code_style = {
+	comments = 'italic',
+	functions = 'bold'
+    }
+}
+require('onedark').load()
---
+++ nvim/lua/oleg/init.lua
@@ -0,0 +1,46 @@
+require("oleg.remap")
+require("oleg.set")
+
+local augroup = vim.api.nvim_create_augroup
+local OlegGroup = augroup('Oleg', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = OlegGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+autocmd({ "FocusGained" }, {
+	pattern = { "*" },
+	command = [[call setreg("@", getreg("+"))]],
+})
+
+autocmd({ "FocusLost" }, {
+	pattern = { "*" },
+	command = [[call setreg("+", getreg("@"))]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+-- Use the system clipboard
+-- vim.opt.clipboard = 'unnamedplus'
